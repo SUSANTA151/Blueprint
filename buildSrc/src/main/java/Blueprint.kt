0f96b09b62256f250b586d6a0e5e12b88facbf87
SLJ-DESIGNS <sljdesigns.skr@gmail.com>
2023-10-09 16:44:57 +0530

    Initialize project

---
+++ buildSrc/src/main/java/Blueprint.kt
@@ -0,0 +1,7 @@
+@file:Suppress("unused")
+
+object Blueprint {
+    const val appId = "dev.jahir.blueprint.app"
+    const val version = 239
+    const val versionName = "2.3.9"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,23 @@
+@file:Suppress("unused")
+
+object Versions {
+    // Plugins
+    const val gradle = "8.1.1"
+    const val kotlin = "1.9.10"
+    const val sonatype = "1.3.0"
+    const val ksp = "$kotlin-1.0.13"
+
+    // OneSignal
+    const val oneSignal = "4.8.6"
+
+    // App
+    const val minSdk = 21
+    const val targetSdk = 34
+    const val buildTools = "34.0.0"
+
+    // Kuper
+    const val kuper = "2.4.9"
+
+    // Adaptive Icons
+    const val adaptiveIcons = "1.0.1"
+}
