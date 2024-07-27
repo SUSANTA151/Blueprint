9d4bb97bfb796d2fda9feb8af8c9ca0753b4f1d8
SLJ-DESIGNS <sljdesigns.skr@gmail.com>
2024-07-27 19:51:35 +0530

    Minor improvement

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,13 +2,13 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.1.1"
-    const val kotlin = "1.9.10"
-    const val sonatype = "1.3.0"
-    const val ksp = "$kotlin-1.0.13"
+    const val gradle = "8.5.0"
+    const val kotlin = "2.0.0"
+    const val sonatype = "2.0.0"
+    const val ksp = "$kotlin-1.0.22"
 
     // OneSignal
-    const val oneSignal = "4.8.6"
+    const val oneSignal = "4.8.10"
 
     // App
     const val minSdk = 21
@@ -16,7 +16,7 @@ object Versions {
     const val buildTools = "34.0.0"
 
     // Kuper
-    const val kuper = "2.4.9"
+    const val kuper = "2.5.2"
 
     // Adaptive Icons
     const val adaptiveIcons = "1.0.1"
